1b09dc7a14bbedd9b5e56aa75f65def922e0cc8a
Clifford Armstrong III <clifford.armstrong.3@gmail.com>
2018-11-08 11:41:52 -0800

    init framework

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,12 +8,12 @@ describe("routes : static", () => {
   describe("GET /", () => {
 
 // Return code 200
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome!' in the body of the response", (done) => {
 
 // Get base url and second argument catches response properties
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
+        expect(body).toContain("Welcome!");
 // Call done to let jasmine know test is over
         done();
       });
