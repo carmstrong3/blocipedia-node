a8f322d98d4809ceddb45d3c74be54a8d16aae95
Clifford Armstrong III <clifford.armstrong.3@gmail.com>
2018-11-27 11:52:41 -0800

    changed app

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,5 +1,5 @@
 const wikiQueries = require("../db/queries.wikis.js");
-const Authorizer = require("../policies/wiki");
+const Authorizer = require("../policies/application");
 const markdown = require("markdown").markdown;
 
 module.exports = {
